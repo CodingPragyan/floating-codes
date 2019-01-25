288df17acaff5aa0dd26d71111ce32c83292ff7a
CodingPragyan <47042372+CodingPragyan@users.noreply.github.com>
2019-01-26 02:17:48 +0530

    Add files via upload
    Simple command-line Book Store Application using Pymongo

---
+++ BookStore.py
@@ -0,0 +1,11 @@
+class BookStore:
+    def __init__(self, title, isbn, author_id, publisher_id, price, date):
+        self.title = title
+        self.isbn = isbn
+        self.author_id = author_id
+        self.publisher_id = publisher_id
+        self.price = price
+        self.date = date
+
+
+
